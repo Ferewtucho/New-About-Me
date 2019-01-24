6157451891ff2377a8004aa43adefc382d93cd79
Ferew Tucho <ferewtucho@gmail.com>
2019-01-24 11:07:47 -0500

    change some effects

--- assets/js/time.js
+++ assets/js/time.js
@@ -3,13 +3,27 @@ var hourNow = date.getHours();
 var greeting;
 
 if (hourNow > 18) {
-    greeting = 'Good evening!';
+  greeting = "Good evening!";
 } else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
+  greeting = "Good afternoon!";
 } else if (hourNow > 0) {
-    greeting = 'Good morning!';
+  greeting = "Good morning!";
 } else {
-    greeting = 'Welcome!';
+  greeting = "Welcome!";
 }
 
-document.getElementById("greeting").innerHTML = greeting ;
+document.getElementById("greeting").innerHTML = greeting;
+
+// Scroll effect jquery for nav
+
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("scrollNav");
+    $(".navbar-header").addClass("navbar-brand2");
+    $("ul").addClass(".liC");
+  } else {
+    $("nav").removeClass("scrollNav");
+    $(".navbar-header").removeClass("navbar-brand2");
+    $("ul").removeClass(".liC");
+  }
+});
