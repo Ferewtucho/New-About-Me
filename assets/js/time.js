c6a1b2b08169dab3a2e77af6f57de801e84ac3a3
Ferew Tucho <ferewtucho@gmail.com>
2019-02-12 17:59:52 -0500

    main update animation text color resume

--- assets/js/time.js
+++ assets/js/time.js
@@ -1,18 +1,18 @@
-var date = new Date();
-var hourNow = date.getHours();
-var greeting;
+// var date = new Date();
+// var hourNow = date.getHours();
+// var greeting;
 
-if (hourNow > 18) {
-  greeting = "Good evening!";
-} else if (hourNow > 12) {
-  greeting = "Good afternoon!";
-} else if (hourNow > 0) {
-  greeting = "Good morning!";
-} else {
-  greeting = "Welcome!";
-}
+// if (hourNow > 18) {
+//   greeting = "Good evening!";
+// } else if (hourNow > 12) {
+//   greeting = "Good afternoon!";
+// } else if (hourNow > 0) {
+//   greeting = "Good morning!";
+// } else {
+//   greeting = "Welcome!";
+// }
 
-document.getElementById("greeting").innerHTML = greeting;
+// document.getElementById("greeting").innerHTML = greeting;
 
 // Scroll effect jquery for nav
 
